234c5924cb014fa27524b15663fb7f4ed5c4ef01
Tan Chin Siang <tan.chinsiang@tmrnd.com.my>
2023-06-30 12:24:17 +0800

    completed task 5, to get book review based on ISBN

--- router/general.js
+++ router/general.js
@@ -62,7 +62,16 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+
+  if(isbn > books.length){
+    return res.status(400).json({
+      message: "Invalid isbn value"
+    })
+  }
+
+  const filtered = books[+isbn -1]
+  return res.status(200).json({bookReview: filtered.reviews});
 });
 
 module.exports.general = public_users;
